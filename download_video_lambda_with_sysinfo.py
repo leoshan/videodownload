c770126d5f05ffbe9b21c2bdf31f07c7b642ead1
Leo <leoshch@gmail.com>
2018-12-28 06:26:05 +0000

    Create download_video_lambda_with_sysinfo.py

---
+++ download_video_lambda_with_sysinfo.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import wget # pip install wget
+import boto3 # pip install boto3
+import json
+import logging
+import os
+from boto3.dynamodb.conditions import Key, Attr
+
+def download_video(event,context):
+    uname = os.uname()
+    print(uname)
+    
+    p = os.popen('uptime',"r")
+    while 1:
+        line = p.readline()
+        if not line:break
+        print(line) 
+    
+    p = os.popen('cat /proc/meminfo|grep -E "MemTotal|MemAvailable"',"r")
+    while 1:
+        line = p.readline()
+        if not line:break
+        print(line)
+    
+    p = os.popen('cat /proc/cpuinfo | grep "model name"',"r")
+    while 1:
+        line = p.readline()
+        if not line:break
+        print(line)
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    s3_client = boto3.client('s3')
+
+    table = dynamodb.Table('videolist')
+    id = event['id']
+    response = table.query(
+        IndexName='id-video_name-index',
+        KeyConditionExpression=Key('id').eq(id)
+    )
+    #print(response)
+    if response['Items']:
+        for item in response['Items']:
+            #print(item)
+            url = item['download_url']
+            video_name = item['video_name']
+            str_videoname = item['video_name']
+            out_fname = "/tmp/video_"+item['id_bykey']+".mp4"
+            filename = wget.download(url,out_fname)
+            print('Downloaded video id is: ',bytes(id),'; filename is: ', filename)
+            upload_path = filename
+            key = "video1k/video_"+item['id_bykey']+".mp4"
+            bucket = 'video-uw2'
+            s3_client.upload_file(upload_path, bucket, key)
+            print('Upload video id is: ',bytes(id),'; filename is: ', filename,'; video name is: ', str_videoname)
+    else:
+        print('id',bytes(id),'is not exist') 
+            
